d59799666ad0ee8934d853aa0af526edf41a31ca
luotao-pc <luotao@okay.cn>
2018-12-12 20:54:54 +0800

    3213

--- command.bat
+++ command.bat
@@ -1,2 +1,4 @@
 .\bin\windows\kafka-server-start.bat .\config\server.properties
+.\bin\windows\kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic zipkin
+.\bin\windows\kafka-topics.bat --list --zookeeper localhost:2181
 
